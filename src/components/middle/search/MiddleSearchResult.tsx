b545b03fefa0feedae44dbac71e50e1dc4ae04dd
Roman Mendaliev <reacheight8@gmail.com>
2025-09-30 21:45:03 +0300

    better message prview in search

--- src/components/middle/search/MiddleSearchResult.tsx
+++ src/components/middle/search/MiddleSearchResult.tsx
@@ -1,13 +1,24 @@
 import { memo } from '../../../lib/teact/teact';
+import { getActions, withGlobal } from '../../../global';
 
-import type { ApiChat, ApiMessage, ApiPeer } from '../../../api/types';
+import type { ApiChat, ApiMessage, ApiPeer, ApiWebPage } from '../../../api/types';
 
+import {
+  getMessageIsSpoiler,
+  getMessageRoundVideo,
+  getMessageSticker,
+  getMessageVideo,
+} from '../../../global/helpers';
 import { getMessageSenderName } from '../../../global/helpers/peers';
+import { selectWebPageFromMessage } from '../../../global/selectors';
 import buildClassName from '../../../util/buildClassName';
 import renderText from '../../common/helpers/renderText';
 
+import useMessageMediaHash from '../../../hooks/media/useMessageMediaHash';
+import useThumbnail from '../../../hooks/media/useThumbnail';
 import useLang from '../../../hooks/useLang';
 import useLastCallback from '../../../hooks/useLastCallback';
+import useMedia from '../../../hooks/useMedia';
 
 import Avatar from '../../common/Avatar';
 import FullNameTitle from '../../common/FullNameTitle';
@@ -27,6 +38,10 @@ type OwnProps = {
   onClick: (message: ApiMessage) => void;
 };
 
+type StateProps = {
+  webPage?: ApiWebPage;
+};
+
 const TRUNCATE_LENGTH = 200;
 
 const MiddleSearchResult = ({
@@ -38,7 +53,8 @@ const MiddleSearchResult = ({
   query,
   className,
   onClick,
-}: OwnProps) => {
+  webPage,
+}: OwnProps & StateProps) => {
   const lang = useLang();
   const hiddenForwardTitle = message.forwardInfo?.hiddenUserName;
 
@@ -46,8 +62,40 @@ const MiddleSearchResult = ({
 
   const senderName = shouldShowChat && senderPeer ? getMessageSenderName(lang, message.chatId, senderPeer) : undefined;
 
+  const thumbDataUri = useThumbnail(message);
+  const mediaThumbnail = !getMessageSticker(message) ? thumbDataUri : undefined;
+  const mediaHash = useMessageMediaHash(message, 'pictogram');
+  const mediaBlobUrl = useMedia(mediaHash);
+  const isRoundVideo = Boolean(getMessageRoundVideo(message));
+  const hasVideo = Boolean(getMessageVideo(message));
+  const isSpoiler = getMessageIsSpoiler(message);
+  const hasMediaPreview = Boolean(mediaBlobUrl || mediaThumbnail);
+
+  const webPageInfo = webPage?.webpageType === 'full' ? {
+    siteName: webPage.siteName || webPage.displayUrl,
+    title: webPage.title,
+    url: webPage.url,
+    isSafe: message.content.webPage?.isSafe,
+  } : undefined;
+
   const handleClick = useLastCallback(() => {
     onClick(message);
+  });
+
+  const handleWebPageClick = useLastCallback((e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (!webPageInfo?.url) return;
+
+    const { openUrl } = getActions();
+    openUrl({ url: webPageInfo.url, shouldSkipModal: webPageInfo.isSafe });
+  });
+
+  const handleMediaClick = useLastCallback((e: React.MouseEvent) => {
+    if (webPageInfo?.url) {
+      e.stopPropagation();
+      const { openUrl } = getActions();
+      openUrl({ url: webPageInfo.url, shouldSkipModal: webPageInfo.isSafe });
+    }
   });
 
   return (
@@ -77,9 +125,53 @@ const MiddleSearchResult = ({
           )}
           <MessageSummary message={message} highlight={query} truncateLength={TRUNCATE_LENGTH} />
         </div>
+        {webPageInfo && (
+          <div
+            className={styles.webPageInfo}
+            onClick={handleWebPageClick}
+            role="button"
+            tabIndex={0}
+            title={webPageInfo.url}
+          >
+            <div className={styles.webPageSite}>{renderText(webPageInfo.siteName)}</div>
+            {webPageInfo.title && (
+              <div className={styles.webPageTitle}>{renderText(webPageInfo.title)}</div>
+            )}
+          </div>
+        )}
       </div>
+      {hasMediaPreview && (
+        <div
+          className={buildClassName(
+            styles.mediaPreview,
+            webPageInfo && styles.clickable,
+          )}
+          onClick={handleMediaClick}
+          role={webPageInfo ? 'button' : undefined}
+          tabIndex={webPageInfo ? 0 : undefined}
+          title={webPageInfo?.url}
+        >
+          <img
+            src={mediaBlobUrl || mediaThumbnail}
+            alt=""
+            className={buildClassName(
+              styles.mediaImage,
+              isRoundVideo && styles.round,
+              isSpoiler && styles.spoiler,
+            )}
+            draggable={false}
+          />
+          {hasVideo && <i className={buildClassName(styles.playIcon, 'icon icon-play')} />}
+        </div>
+      )}
     </div>
   );
 };
 
-export default memo(MiddleSearchResult);
+export default memo(withGlobal<OwnProps>(
+  (global, { message }): Complete<StateProps> => {
+    return {
+      webPage: selectWebPageFromMessage(global, message),
+    };
+  },
+)(MiddleSearchResult));
